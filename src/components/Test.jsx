5eddcacaf66e363864a20090db9e9c81ec7cf457
raghav512 <raghav@krishigyanai.com>
2026-06-08 14:29:37 +0530

    testing

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,10 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const Test = () => {
   return (
     <View>
-      <Text>Test</Text>
+      <Text>Raghav Gupta</Text>
     </View>
   );
 };
